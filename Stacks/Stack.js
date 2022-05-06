883b342654d937f6d58e0cd2a75ffc9bf65e562c
bignichok <inikita546@gmail.com>
2022-05-06 12:25:10 +0300

    stack implementation

---
+++ Stacks/Stack.js
@@ -0,0 +1,57 @@
+//we can create stack with array but it is not good because of bigO complexity of adding or removing from the beginning of array
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+class Stack{
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    push(value){
+        const newNode = new Node(value)
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            newNode.next =  this.first;
+            this.first = newNode;
+        }
+        this.size++
+        return this;
+    }
+
+    pop(){
+        if(!this.first) return null;
+        const poppedNode = this.first
+        this.first = this.first.next;
+        this.size--
+        if(this.size === 1) this.last = null;
+        return poppedNode;
+    }
+
+    print(){
+        let arr = [];
+        let current = this.first;
+        while(current){
+            current = current.next;
+            arr.push(current)
+        }
+        console.log(arr);
+    }
+}
+
+const stack = new Stack()
+
+stack.push(1).push(2).push(3).push(4).push(5);
+stack.print();
+stack.pop();
+stack.print();
+stack.pop();
+stack.pop();
+stack.pop();
+stack.print();
---
+++ Stacks/readme.md
@@ -0,0 +1,10 @@
+##Stacks
+
+- LIFO
+- The last element added to the stack will be the first element removed from the stack
+
+###stacks use cases
+
+- managing function invocations
+- Undo/Redo
+- Routing(the history object) is treated like a stack!
