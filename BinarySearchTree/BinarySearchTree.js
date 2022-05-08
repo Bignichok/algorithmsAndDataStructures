4425cf6c8b5b3c2117e51ed96f3506c25cd97620
Bignichok <inikita546@gmai.com>
2022-05-08 17:43:34 +0300

    BinarySearchTree

---
+++ BinarySearchTree/BinarySearchTree.js
@@ -0,0 +1,66 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+        this.count = 1;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        }
+        let current = this.root;
+        while (true) {
+            if (value === current.value) {
+                current.count++;
+                return this;
+            }
+            if (value > current.value) {
+                if (!current.right) {
+                    current.right = newNode;
+                    return this;
+                } else {
+                    current = current.right;
+                }
+            } else {
+                if (!current.left) {
+                    current.left = newNode;
+                    return this;
+                } else {
+                    current = current.left;
+                }
+            }
+        }
+    }
+
+    find(value) {
+        if (!this.root) {
+            return false;
+        }
+
+        let current = this.root;
+        let found = false;
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+        if (!found) return false;
+        return current;
+    }
+}
+
+const binarySearchTree = new BinarySearchTree();
---
+++ BinarySearchTree/readme.md
@@ -0,0 +1,24 @@
+##Trees
+
+-   Root - the top node in a tree.
+-   Child - a node directly connected to another node when moving away from the Root.
+-   Parent - the converse notion of a child.
+-   Siblings - group of nodes with the same parent.
+-   Leaf - a node with no children.
+-   Edge - the connection between one node and another.
+
+###Binary search trees
+
+-   Every parent node has at most two children
+-   Every node to the left of a parent node is always less than the parent
+-   Every node to the right of a parent node is always greater than the parent
+-   O (log n) in average case
+
+###Use cases
+
+-   HTML DOM
+-   Network routing
+-   Abstract syntax tree
+-   Artificial Intelligence
+-   Folders in Operating Systems
+-   Computer file system
